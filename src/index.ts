7cc3149bf5339f66f1f66172aaea94d183864ab7
tintinthong <justinthong93@gmail.com>
2020-04-04 18:59:23 +0800

    Added more features. Scale is not working properly

--- src/index.ts
+++ src/index.ts
@@ -5,25 +5,79 @@ export class Vector {
     ) {
         this.v = v
     }
-    sum():number {
-        return this.v.reduce((sum,n)=>{
-            return sum+n
-        },0)
+    sum(): number {
+        return this.v.reduce((sum, n) => {
+            return sum + n
+        }, 0)
     }
-    len(): number{
-        return this.v.length 
+    len(): number {
+        return this.v.length
     }
-    average(): number{
-        return this.sum()/this.v.length
+    average(): number {
+        return this.sum() / this.v.length
     }
-    add(v2: Vector): Vector{
-        if(this.v.length != v2.len()){
-            throw new Error("Vector not off the same length")
+    add(v2: Vector): Vector {
+        if (this.v.length != v2.len()) {
+            throw new Error(`Mistmatch in dimension: ${this.v.length} and ${v2.len}`)
         }
-        const vout = this.v.map((n,i)=>{
-            return n+v2.v[i]
+        return new Vector(this.v.map((n, i) => {
+            return n + v2.v[i]
+        }))
+    }
+    product(v2: Vector):number[]{
+        return v2.v.map((n, i) => {
+            return n*v2.v[i]
+        }) 
+    }
+    powerSum(a: number):number{
+        return this.v.reduce((sum, n) => {
+            return sum + n^a
+        }, 0) 
+    }
+    closure(v: number[]): number[] {
+        return v.map((n) => {
+            return n / this.sum()
         })
-        return new Vector(vout)
+    }
+}
+
+export class Composition extends Vector {
+    constructor(
+        v: number[],
+        opts: {
+            terse: boolean
+        } = {
+            terse: false
+        }
+    ) {
+        super(v);
+        this.v = (!opts.terse) ? this.closure(this.v) : this.closure(v).slice(0, -1)
+    }
+    scale(a: number):Composition{
+        return new Composition(this.v.map((n)=>{
+            return n^a/this.powerSum(a)
+        }))
+    }
+    add(comp2: Composition): Composition {
+        if (this.v.length != comp2.len()) {
+            throw new Error(`Mistmatch in dimension: ${this.v.length} and ${comp2.len}`)
+        }
+        return new Composition(this.v.map((n, i) => {
+            return n + comp2.v[i]
+        }))
+    }
+    perturb(comp2: Composition): Composition {
+        if (this.v.length != comp2.len()) {
+            throw new Error(`Mistmatch in dimension: ${this.v.length} and ${comp2.len}`)
+        }
+        const prodVec = this.product(comp2) 
+        const tot = prodVec.reduce((sum, n)=>{
+            return n+sum
+        },0)
+        return new Composition(prodVec.map((n)=>{
+            return n/tot
+        }))
+
     }
 
 }
---
+++ tests/composition.js
@@ -0,0 +1,20 @@
+
+const {Composition} = require('../dist/index');
+
+// All methods inherited from Vector
+const v1 = new Composition([1,2,3,4])
+const v2 = new Composition([2,2,3,4])
+
+console.log(v1);
+console.log(v2);
+console.log(v1.sum());
+console.log(v1.average());
+console.log(v1.len(v2))
+console.log(v1.add(v2))
+
+console.log(v1.constructor.name)
+console.log(v2.constructor.name)
+console.log(v1.add(v2).constructor.name)
+
+console.log(v1.scale(10))
+console.log(v1.perturb(v2))
